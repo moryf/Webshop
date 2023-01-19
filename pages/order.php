6e95dfee9a2f8fb9f5af7cdb43c1ab0e1c7932e9
Aleksa Djordjiev <aleksa.kb24@live.com>
2023-01-19 12:29:09 +0100

    Order file added

---
+++ pages/order.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+require("../util/baza_conn.php");
+require("../modeli/proizvod.php");
+
+$id = $_SESSION['user_id'];
+$product_id = $_SESSION['order'];
+
+$query = "SELECT * FROM proizvodi WHERE id=$product_id";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_assoc($result);
+
+$product = new Proizvod($row['id'], $row['prodavnica'], $row['ime'], $row['cena'], $row['opis']);
+
+if (isset($_POST['submit'])) {
+    $product_id = $_POST['product_id'];
+  
+    $query = "INSERT INTO porudzbine VALUES ($id,$product_id)";
+    $result = mysqli_query($conn,$query);
+  
+      header('Location: shop.php');
+      exit;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../style/shop_style.css">  
+    <link rel="stylesheet" type="text/css" href="../style/home_style.css"> 
+    <title>Document</title>
+</head>
+<body>
+    <h1>Potvrdite Porudžbinu</h1>
+    <div class="product-grid">
+    <?php
+    echo '<div class="product-card">';
+    echo '  <img src="../content/basket.png">';
+    echo '  <h3>' . $product->ime . '</h3>';
+    echo '  <p>' . $product->opis . '</p>';
+    echo '  <p>' . $product->cena . '</p>';
+    echo '  <form method="post">';
+    echo '    <input type="hidden" name="product_id" value="' . $product->id . '">';
+    echo '    <input type="submit" class="btn" name="submit" value="Confirm order">';
+    echo '  </form>';
+    echo '</div>';
+    
+    ?>
+    </div>
+    
+</body>
+</html>
